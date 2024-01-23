e73be1b9f4676c5371926fd71b5140df9145de15
e1000son <emilsonvontade@gmail.com>
2024-01-23 11:33:02 +0200

    Handling user lagin

--- dnrn/src/main/java/mz/gov/inage/dnrn/controllers/UserController.java
+++ dnrn/src/main/java/mz/gov/inage/dnrn/controllers/UserController.java
@@ -4,9 +4,11 @@ import mz.gov.inage.dnrn.entities.User;
 import mz.gov.inage.dnrn.repos.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
@@ -23,4 +25,19 @@ public class UserController {
 
     @RequestMapping("/showLogin")
     public String showLoginPage(){return "/auth/login";}
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String login(@RequestParam("email") String email,
+                        @RequestParam("password") String password,
+                        ModelMap modelMap){
+        //Lançar e tratar excepção
+        User user = userRepository.findByEmail(email);
+
+        if (user.getPassword().equals(password))
+            return "/citizen/registerCitizen";
+        else
+            modelMap.addAttribute("msg",
+                    "Nome do utilizador ou Password inválidos. " +
+                            "Por favor, tente novamente.");
+        return showLoginPage();
+    }
 }
